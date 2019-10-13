faa181dcb26695dd526a3fa376cd04f559c5309e
szymon961113 <szymon961113@gmail.com>
2019-10-13 09:57:57 +0200

    Initial Api

---
+++ src/main/java/AI/dtapijava/DtApiJavaApplication.java
@@ -0,0 +1,13 @@
+package AI.dtapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtApiJavaApplication.class, args);
+	}
+
+}
