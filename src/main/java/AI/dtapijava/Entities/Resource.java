f94267f3f3ee5770f9ef950cbf3df081b0f043cb
sebastianwisniowski <sebastiw@wp.pl>
2019-10-21 21:00:25 +0200

    Tym razem serio utworzenie klasy entity dla tabeli Resources

--- src/main/java/AI/dtapijava/Entities/Resource.java
+++ src/main/java/AI/dtapijava/Entities/Resource.java
@@ -1,4 +1,36 @@
 package AI.dtapijava.Entities;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@Table(schema = "public", name = "Resources")
 public class Resource {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Integer ID;
+
+    @ManyToOne (targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn (name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne (targetEntity = Company.class, fetch = FetchType.EAGER)
+    @JoinColumn (name = "comp_id", nullable = false)
+    private Company comp;
+   
+
+
+    @Column(nullable = false)
+    private Integer amount;
+
+
 }
