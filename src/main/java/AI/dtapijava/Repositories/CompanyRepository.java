abcf8a55ecf6d0413a23b94988400f043f36ee58
DawidKalandyk <dawid.kalandyk@gmail.com>
2019-10-22 13:39:36 +0200

    Dodano repozytoria (a przynajmniej ich szkielety)

--- src/main/java/AI/dtapijava/Repositories/CompanyRepository.java
+++ src/main/java/AI/dtapijava/Repositories/CompanyRepository.java
@@ -2,6 +2,8 @@ package AI.dtapijava.Repositories;
 
 import AI.dtapijava.Entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 }
