69ff9d5639a6dd941ced5bc1f2db7ec756779ec1
Sebastian Wawro <user6917@gmail.com>
2019-11-12 18:26:12 +0100

    Proper forwarding for front-end

---
+++ src/main/java/AI/dtapijava/Controllers/ForwardController.java
@@ -0,0 +1,20 @@
+package AI.dtapijava.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+    @RequestMapping("/dt-app")
+    public String forwarda() {
+        return "forward:/dt-app/index.html";
+    }
+    @RequestMapping("/dt-app/")
+    public String forwardb() {
+        return "forward:/dt-app/index.html";
+    }
+    @RequestMapping("/dt-app/{path:[^\\.]+}/**")
+    public String forwardc() {
+        return "forward:/dt-app/index.html";
+    }
+}
