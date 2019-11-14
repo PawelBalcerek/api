0e9d7753f3e14b0b50ac1187a83d4c540b55621d
DawidKalandyk <dawid.kalandyk@gmail.com>
2019-11-14 15:13:54 +0100

    Next part of trade logic and some bugs fixed

--- src/main/java/AI/dtapijava/Services/TradeService.java
+++ src/main/java/AI/dtapijava/Services/TradeService.java
@@ -32,21 +32,48 @@ public class TradeService {
     private TransactionRepository transactionRepository;
 
     private synchronized void startInnerThread(int companyId) {
+
+        System.out.println("Weszło do metody startInnerThread..");
+
         Configuration tableSizeConf = configurationRepository.findById("tableSize").orElse(new Configuration("tableSize", 5));
         Pageable page = PageRequest.of(0, tableSizeConf.getNumber());
         while (true) {
+            System.out.println("Weszło do whila..");
+            System.out.println("Budowanie nowej tabeli..");
             List<SellOffer> sellOffers = sellOfferRepository.getAllPendingSellOffersForCompanyId(companyId, page);
             List<BuyOffer> buyOffers = buyOfferRepository.getAllPendingBuyOffersForCompanyId(companyId, page);
+
+
+            System.out.println("SellOffers:");
+            sellOffers.forEach(x->System.out.println(x.getID()+"  "+x.getAmount()+"  "+x.getPrice()));
+            System.out.println("BuyOffers:");
+            buyOffers.forEach(x->System.out.println(x.getID()+"  "+x.getAmount()+"  "+x.getMaxPrice()));
+            System.out.println("Wykonało zapytania o listy bieżących ofert kupna i sprzedaży..");
+
             if (sellOffers.size() != tableSizeConf.getNumber() || buyOffers.size() != tableSizeConf.getNumber()) {
+
+                System.out.println("Liczba ofer kupna bądź sprzedaży za krótka więc return..");
+
                 return;
             }
             int indexSell = 0;
             int indexBuy = 0;
             while (true) {
-                if (indexBuy > buyOffers.size() || indexSell > sellOffers.size()) break;
+                if (indexBuy >= buyOffers.size() || indexSell >= sellOffers.size()) break;
                 if (buyOffers.get(indexBuy).getMaxPrice() >= sellOffers.get(indexSell).getPrice()) {
+
+                    System.out.println("Można przeprowadzić transakcję bo ceny pasują..");
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
                     int amountToSell = sellOffers.get(indexSell).getAmount();
                     int amountToBuy = buyOffers.get(indexBuy).getAmount();
+
+                    System.out.println("amountToBuy: " + amountToBuy + "   amountToSell: " + amountToSell );
+
                     if (amountToBuy > amountToSell) {
                         int transactionAmount = amountToSell;
                         Transaction transaction = Transaction.builder()
@@ -58,8 +85,10 @@ public class TradeService {
                                 .build();
                         sellOffers.get(indexSell).setAmount(sellOffers.get(indexSell).getAmount()-transactionAmount);
                         buyOffers.get(indexBuy).setAmount(buyOffers.get(indexBuy).getAmount()-transactionAmount);
+                        sellOfferRepository.save(sellOffers.get(indexSell));
+                        buyOfferRepository.save(buyOffers.get(indexBuy));
                         transactionRepository.save(transaction);
-                        indexSell++;
+                        ++indexSell;
                     }
                     else if (amountToBuy < amountToSell) {
                         int transactionAmount = amountToBuy;
@@ -72,8 +101,10 @@ public class TradeService {
                                 .build();
                         sellOffers.get(indexSell).setAmount(sellOffers.get(indexSell).getAmount()-transactionAmount);
                         buyOffers.get(indexBuy).setAmount(buyOffers.get(indexBuy).getAmount()-transactionAmount);
+                        sellOfferRepository.save(sellOffers.get(indexSell));
+                        buyOfferRepository.save(buyOffers.get(indexBuy));
                         transactionRepository.save(transaction);
-                        indexBuy++;
+                        ++indexBuy;
                     }
                     else {
                         int transactionAmount = amountToBuy;
@@ -86,9 +117,11 @@ public class TradeService {
                                 .build();
                         sellOffers.get(indexSell).setAmount(sellOffers.get(indexSell).getAmount()-transactionAmount);
                         buyOffers.get(indexBuy).setAmount(buyOffers.get(indexBuy).getAmount()-transactionAmount);
+                        sellOfferRepository.save(sellOffers.get(indexSell));
+                        buyOfferRepository.save(buyOffers.get(indexBuy));
                         transactionRepository.save(transaction);
-                        indexBuy++;
-                        indexSell++;
+                        ++indexBuy;
+                        ++indexSell;
                     }
                 }
                 else break;
