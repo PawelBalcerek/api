52903df3087c58a5e23f42d62f3ea22b8c6bfa33
Sebastian Wawro <user6917@gmail.com>
2019-12-02 15:15:13 +0100

    Real trade logic fix // I GAVE YOU MY HEART
    you read the code at your own responsibility

--- src/main/java/AI/dtapijava/Controllers/CompanyController.java
+++ src/main/java/AI/dtapijava/Controllers/CompanyController.java
@@ -1,10 +1,15 @@
 package AI.dtapijava.Controllers;
 
+import AI.dtapijava.Components.ExecDetailsHelper;
 import AI.dtapijava.DTOs.Request.CompanyCreateReqDTO;
 import AI.dtapijava.DTOs.Response.CompanyInfoResDTO;
 import AI.dtapijava.DTOs.Response.CompanyResDTO;
+import AI.dtapijava.DTOs.Response.ExecDetailsResDTO;
 import AI.dtapijava.DTOs.Response.ExecTimeResDTO;
 import AI.dtapijava.Services.CompanyService;
+import AI.dtapijava.Services.TradeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +19,12 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api")
 public class CompanyController {
+    private final Logger log = LoggerFactory.getLogger(CompanyController.class);
 
     @Autowired
     private CompanyService companyService;
+    @Autowired
+    private TradeService tradeService;
 
     @PostMapping("/companies")
     public ResponseEntity<ExecTimeResDTO> createCompany(@Valid @RequestBody CompanyCreateReqDTO companyCreateReqDTO) {
@@ -27,6 +35,14 @@ public class CompanyController {
     @GetMapping("/companies/{id}")
     public ResponseEntity<CompanyResDTO> getCompany(@PathVariable int id) {
         return ResponseEntity.ok(new CompanyResDTO(companyService.getCompany(id)));
+    }
+
+    @GetMapping("/tradeForce/{id}")
+    public ResponseEntity<ExecTimeResDTO> tradeForce(@PathVariable int id) {
+        ExecDetailsHelper execDetailsHelper = new ExecDetailsHelper();
+        log.debug("Forced trade for company: " + id);
+        tradeService.startThread(id);
+        return ResponseEntity.ok(new ExecTimeResDTO(new ExecDetailsResDTO(0, execDetailsHelper.getExecTime())));
     }
 
     @GetMapping("/companies")
--- src/main/java/AI/dtapijava/Entities/Resource.java
+++ src/main/java/AI/dtapijava/Entities/Resource.java
@@ -2,6 +2,7 @@ package AI.dtapijava.Entities;
 
 
 import lombok.*;
+import org.hibernate.annotations.Proxy;
 
 import javax.persistence.*;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(schema = "public", name = "resources")
+@Proxy(lazy = false)
 public class Resource {
 
     @Id
